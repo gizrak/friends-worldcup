fc519aabd3ac5ee78fc66e9007a29c0d12801088
Fred Hwang <doodyfree@gmail.com>
2014-03-19 00:11:55 +0900

    rename as worldcup

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
+var redis = require('redis'),
+	client = redis.createClient();
 
-/*
+/**
  * GET home page.
  */
+exports.index = function(req, res) {
+    client.on('error', function(err) {
+        console.log('Error: ' + err);
+    });
 
-exports.index = function(req, res){
-  res.render('index', { title: '절친 월드컵' });
+    client.set('string key', 'string val', redis.print);
+    client.hset('hash key', 'hashtest 1', 'some value', redis.print);
+    client.hset(['hash key', 'hashtest 2', 'some other value'], redis.print);
+    client.hkeys('hash key', function(err, replies) {
+        console.log(replies.length + ' replies: ');
+        replies.forEach(function(reply, i) {
+            console.log('    ' + i + ': ' + reply);
+        });
+        client.quit();
+    });
+
+    res.render('index', { title: '절친 월드컵' });
 };
